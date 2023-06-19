c0dafcfd5bf019d0496a0c42a89dadec10f04dea
Phat Vo <phat@briohr.com>
2023-06-19 17:04:50 +0700

    Add real Open API Key, update testing and GitHub CI

--- libs/common/src/services/language-models/lang-chain.ts
+++ libs/common/src/services/language-models/lang-chain.ts
@@ -1,7 +1,7 @@
 import { DynamicModule, Module } from '@nestjs/common';
 import { BaseChatModel } from 'langchain/chat_models';
 import { ChatOpenAI } from 'langchain/chat_models/openai';
-// import { BaseChatMessage, HumanChatMessage } from 'langchain/schema';
+import { HumanChatMessage } from 'langchain/schema';
 
 @Module({})
 export class LangChain {
@@ -19,9 +19,8 @@ export class LangChain {
     };
   }
 
-  // TODO: Since the ChatGPT API Key is required to be paid, so need to have a valid key to send the message
-  public static async chat(message: string): Promise<any> {
-    return `That is good mate ${message}`;
-    // await this.model.call([new HumanChatMessage(message)]);
+  public static async chat(message: string): Promise<string> {
+    const { text } = await this.model.call([new HumanChatMessage(message)]);
+    return text;
   }
 }
--- libs/common/tests/setup.testing.ts
+++ libs/common/tests/setup.testing.ts
@@ -1,15 +1,24 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import * as dotenv from 'dotenv';
+import * as process from 'process';
+
+dotenv.config({
+  path: 'apps/chat-api/.env',
+});
+
 process.env.NODE_ENV = 'testing';
 // eslint-disable-next-line no-secrets/no-secrets
-process.env.DATABASE_URL = 'mongodb://mongo:mongo@localhost:27017/chat-api?authSource=admin';
+process.env.DATABASE_URL =
+  process.env.DATABASE_URL ?? 'mongodb://mongo:mongo@localhost:27017/chat-api?authSource=admin';
 
-process.env.THROTTLE_TTL = '60';
-process.env.THROTTLE_LIMIT = '10';
+process.env.THROTTLE_TTL = process.env.THROTTLE_TTL ?? '60';
+process.env.THROTTLE_LIMIT = process.env.THROTTLE_LIMIT ?? '10';
 
-process.env.JWT_KEY = 'thisislongjwtkeyconfiguration';
-process.env.REFRESH_TOKEN_TTL_LONG_LIVED = '2_592_000';
-process.env.REFRESH_TOKEN_TTL = '86_400';
-process.env.JWT_TTL = '50000';
+process.env.JWT_KEY = process.env.JWT_KEY ?? 'thisislongjwtkeyconfiguration';
+process.env.REFRESH_TOKEN_TTL_LONG_LIVED = process.env.REFRESH_TOKEN_TTL_LONG_LIVE ?? '2_592_000';
+process.env.REFRESH_TOKEN_TTL = process.env.REFRESH_TOKEN_TTL ?? '86_400';
+process.env.JWT_TTL = process.env.JWT_TTL ?? '50000';
 
-process.env.OPEN_API_KEY = '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';
+process.env.OPEN_API_KEY = process.env.OPEN_API_KEY ?? '';
 
 jest.setTimeout(60_000);
